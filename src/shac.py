29923c712e5b7ad64558ac59dd7b9ee6c1640301
pizza <pizza@extracheese.dyn.optonline.net>
2009-04-13 03:48:16 -0400

    shuffled stuff around

--- src/shac.py
+++ src/shac.py
@@ -3,6 +3,9 @@
 
 import os
 
+# 
+# 
+# 
 class Perm:
 
   NONE = 0x00
@@ -51,16 +54,87 @@ assert "d"     == Perm().tostr(Perm.Dele)
 assert "r"     == Perm().tostr(Perm.Read)
 assert "rwxcd" == Perm().tostr(Perm.Read | Perm.Writ | Perm.Exec | Perm.Crea | Perm.Dele)
 
+# 
+# 
+# 
+class Reason:
+
+  path  = ""    # 
+  label = 0     # ???
+  yes   = [ ]   # reasons why we CAN access file/dir
+  no    = [ ]   # reasons why we CAN'T
+
+  # reasons why we don't have access
+  class No:
+    Unknown       = 0
+    Read          = 1
+    Write         = 2
+    Exec          = 3
+    Create        = 4
+    Delete        = 5
+    MntPtReadOnly = 6
+    MntPtNoExec   = 7
+    Sticky        = 8
+    Dependency    = 9
+    Str_ = [
+      "Unknown",
+      "NoRead",
+      "NoWrite",
+      "NoExec",
+      "NoCreate",
+      "NoDelete",
+      "MntPtReadOnly",
+      "MntPtNoExec",
+      "Sticky",
+      "Dependency"
+      ]
+    
+    def toStr(self, r):
+      return self.Str_[r]
+
+  class Yes:
+    # reasons why we do have access
+    UserRead   =  0
+    UserWrite  =  1
+    UserExec   =  2
+    GroupRead  =  3
+    GroupWrite =  4
+    GroupExec  =  5
+    OtherRead  =  6
+    OtherWrite =  7
+    OtherExec  =  8
+    Sticky     =  9
+    Owner      = 10
+    Root       = 11
+    Str_ = [
+      "UserRead",
+      "UserWrite",
+      "UserExec",
+      "GroupRead",
+      "GroupWrite",
+      "GroupExec",
+      "OtherRead",
+      "OtherWrite",
+      "OtherExec",
+      "Sticky",
+      "Owner",
+      "Root"
+      ]
+    def toStr(self, r):
+      return self.Str_[r]
+  
+assert "Dependency" == Reason().No().toStr(Reason.No.Dependency)
+assert "Root"       == Reason().Yes().toStr(Reason.Yes.Root)
+
 class File:
   pass
 
-class Path:
-  pass
 
 class Group:
-  pass
+  NONE = -1
 
 class User:
+  NONE = -1
   name = ""
   uid = 0
   groups = [ ]
@@ -80,12 +154,38 @@ class MountPoint:
   def isNoExec(self):
     return True
 
-class Reason:
-  pass
+
+class Path:
+  abspath   = ""
+  dir       = ""
+  component = ""
+  symlink   = ""          # path this file points to if symlink
+  uid       = User.NONE
+  gid       = Group.NONE
+  status    = Perm()
+  mode      = 0 # ???
+  mntpt     = MountPoint()
+
+  def isSymlink(self):
+    pass
+
+  def isDir(self):
+    pass
+
+  def isFile(self):
+    pass
+
+  def isSticky(self):
+    pass
+
+  def isMountPoint(self):
+    pass
+
+  def StatusNotOK(self):
+    pass
 
 
 print "cwd = %s" % os.getcwd()
 print "uid = %d" % os.getuid()
-
 
 
